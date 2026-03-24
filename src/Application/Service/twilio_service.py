d70c864f2c9001c5805dc5648f83aac959ac0d1b
rafaelferracinidev <rafaelferracini@gmail.com>
2026-03-24 16:31:52 -0300

    test: validar fluxo de autenticação e config de ambiente

--- src/Application/Service/twilio_service.py
+++ src/Application/Service/twilio_service.py
@@ -11,7 +11,7 @@ class TwilioService:
 
         sid = os.getenv("TWILIO_ACCOUNT_SID")
         token = os.getenv("TWILIO_AUTH_TOKEN")
-        from_phone = os.getenv("TWILIO_PHONE")
+        from_phone = os.getenv("TWILIO_PHONE_NUMBER")
 
         client = Client(sid, token)
 
